3444f23260232febc7fcc77e679e7083d7b71201
Adrian Kuhn <akuhn@iam.unibe.ch>
2008-03-22 03:14:37 +0000

    A factory making everything.

--- Sources/ch.akuhn.util/src/ch/akuhn/util/License.java
+++ Sources/ch.akuhn.util/src/ch/akuhn/util/License.java
@@ -1,20 +1,19 @@
-//  Copyright (c) 1998-2008 Adrian Kuhn <akuhn(a)iam.unibe.ch>
+//  Copyright (c) 1998-2008 Adrian Kuhn <akuhn(a)students.unibe.ch>
 //  
-//  This file is part of "Adrian Kuhn's Utilities for Java".
+//  This file is part of ch.akuhn.util.
 //  
-//  "Adrian Kuhn's Utilities for Java" is free software: you can redistribute
-//  it and/or modify it under the terms of the GNU Lesser General Public License
-//  as published by the Free Software Foundation, either version 3 of the
-//  License, or (at your option) any later version.
+//  ch.akuhn.util is free software: you can redistribute it and/or modify it
+//  under the terms of the GNU Lesser General Public License as published by the
+//  Free Software Foundation, either version 3 of the License, or (at your
+//  option) any later version.
 //  
-//  "Adrian Kuhn's Utilities for Java" is distributed in the hope that it will
-//  be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
-//  General Public License for more details.
+//  ch.akuhn.util is distributed in the hope that it will be useful, but
+//  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+//  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+//  License for more details.
 //  
 //  You should have received a copy of the GNU Lesser General Public License
-//  along with "Adrian Kuhn's Utilities for Java". If not, see
-//  <http://www.gnu.org/licenses/>.
+//  along with ch.akuhn.util. If not, see <http://www.gnu.org/licenses/>.
 //  
 
 package ch.akuhn.util;
@@ -104,13 +103,13 @@ public class License {
 	}
 
 	public static void main(String... strings) {
-		File root = new File("../ch.unibe.iam.scg.eccrawler/src");
+		File root = new File("src");
 		License l = new License();
-		l.kind = Kind.EPL;
-		l.appName = "ECCrawler";
-		l.author = "Manuel Friedli";
-		l.email = "manuel.friedli(a)students.unibe.ch";
-		l.year = "2007-2008";
+		l.kind = Kind.LESSER_GPL;
+		l.appName = "ch.akuhn.util";
+		l.author = "Adrian Kuhn";
+		l.email = "akuhn(a)students.unibe.ch";
+		l.year = "1998-2008";
 		l.process(root);
 	}
 
