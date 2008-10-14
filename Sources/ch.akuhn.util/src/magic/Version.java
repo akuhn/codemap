d5131c92ed3d9135c985ac09a47a4f885bbf6d7c
Adrian Kuhn <akuhn@iam.unibe.ch>
2008-10-14 18:10:08 +0000

    Moved tests to separate source folder.

---
+++ Sources/ch.akuhn.util/src/magic/Version.java
@@ -0,0 +1,14 @@
+package magic;
+
+public class Version {
+
+	public static void main(String[] args) {
+		System.out.println(number());
+	}
+
+	private static String number() {
+		String id = "$Id$";
+		return id;
+	}
+	
+}
