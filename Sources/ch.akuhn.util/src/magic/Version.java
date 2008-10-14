29d2ba57a907c6e02da15410d70c2997812b858b
Adrian Kuhn <akuhn@iam.unibe.ch>
2008-10-14 18:12:34 +0000

    (Empty commit message)

--- Sources/ch.akuhn.util/src/magic/Version.java
+++ Sources/ch.akuhn.util/src/magic/Version.java
@@ -2,13 +2,16 @@ package magic;
 
 public class Version {
 
+	public static final String AUTHOR = "$author$";
+	public static final String DATE = "$date$";
+	public static final String REVISION = "$revision$";
+	
 	public static void main(String[] args) {
 		System.out.println(number());
 	}
 
 	private static String number() {
-		String id = "$Id$";
-		return id;
+		return REVISION;
 	}
 	
 }
