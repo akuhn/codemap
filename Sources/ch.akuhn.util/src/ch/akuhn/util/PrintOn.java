fcf17b80246284c2e3d70ba1c4e0a2a443713303
Adrian Kuhn <akuhn@iam.unibe.ch>
2008-12-05 07:32:09 +0000

    Class PrintOn

---
+++ Sources/ch.akuhn.util/src/ch/akuhn/util/PrintOn.java
@@ -0,0 +1,75 @@
+package ch.akuhn.util;
+
+import java.io.IOException;
+
+public class PrintOn implements Appendable {
+
+    private Appendable buf;
+
+    public PrintOn() {
+        this(new StringBuilder());
+    }
+    
+    
+    public PrintOn(Appendable buf) {
+        this.buf = buf;
+    }
+
+    public final PrintOn print(int value) {
+        return append(Integer.toString(value));
+    }
+
+    public final PrintOn print(double value) {
+        return append(Double.toString(value));
+    }
+
+    public final PrintOn print(Object object) {
+        return append(String.valueOf(object));
+    }
+    
+    public final PrintOn space() {
+        return append(' ');
+    }
+    
+    public final PrintOn tab() {
+        return append('\t');
+    }
+    
+    public final PrintOn cr() {
+        return append('\n');
+    }
+    
+    public final PrintOn append(CharSequence string) {
+        try {
+            buf.append(string);
+        } catch (IOException ex) {
+            throw Throw.exception(ex);
+        }
+        return this;
+    }
+
+    public final PrintOn append(char c) {
+        try {
+            buf.append(c);
+        } catch (IOException ex) {
+            throw Throw.exception(ex);
+        }
+        return this;
+    }
+
+    public final PrintOn append(CharSequence string, int start, int end) {
+        try {
+            buf.append(string,start,end);
+        } catch (IOException ex) {
+            throw Throw.exception(ex);
+        }
+        return this;
+    }
+
+
+    @Override
+    public String toString() {
+        return buf.toString();
+    }
+
+}
