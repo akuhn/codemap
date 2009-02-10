e70c098728b0732c9a4adb7aad76801c4e28d6d5
David Erni <erni@students.unibe.ch>
2009-02-10 17:09:35 +0000

    changed visualization of case study

--- Students/erni/master/MeanderCaseStudies/src/ch/unibe/jsme2009/JunitCaseStudy.java
+++ Students/erni/master/MeanderCaseStudies/src/ch/unibe/jsme2009/JunitCaseStudy.java
@@ -8,7 +8,17 @@ import ch.akuhn.hapax.corpus.Document;
 import ch.akuhn.hapax.index.LatentSemanticIndex;
 import ch.akuhn.hapax.index.TermDocumentMatrix;
 import ch.akuhn.util.Each;
+import ch.deif.meander.ContourLineAlgorithm;
+import ch.deif.meander.DEMAlgorithm;
+import ch.deif.meander.HillshadeAlgorithm;
+import ch.deif.meander.HillshadeVisualization;
 import ch.deif.meander.MDS;
+import ch.deif.meander.Map;
+import ch.deif.meander.MapBuilder;
+import ch.deif.meander.MapVisualization;
+import ch.deif.meander.NormalizeLocationsAlgorithm;
+import ch.deif.meander.PViewer;
+import ch.deif.meander.SketchVisualization;
 
 
 public class JunitCaseStudy {
@@ -39,9 +49,22 @@ public class JunitCaseStudy {
         MDS mds = MDS.fromCorrelationMatrix(i.documentCorrelation());
         System.out.println(mds.r0);
         System.out.println(mds.r);
+        MapBuilder builder = Map.builder().size(512, 512);
         for (Each<Document> each: withIndex(tdm.documents())) {
-            System.out.printf("%f\t%f\t%s\n", mds.x[each.index], mds.y[each.index], each.element);
+            //System.out.printf("%f\t%f\t%s\n", mds.x[each.index], mds.y[each.index], each.element);
+        	String name = each.element.toString();
+        	if (name.endsWith(("(junit3.4.zip)"))) {
+        		builder.location(mds.x[each.index], mds.y[each.index], Math.sqrt(each.element.terms.size()));        		
+        	}
         }
+        Map map = builder.build();
+        new NormalizeLocationsAlgorithm(map).run();
+        new DEMAlgorithm(map).run();
+        new HillshadeAlgorithm(map).run();
+        new ContourLineAlgorithm(map).run();
+        //MapVisualization viz = new SketchVisualization(map);
+        MapVisualization viz = new HillshadeVisualization(map);
+        new PViewer(viz);        
     }
     
 }
