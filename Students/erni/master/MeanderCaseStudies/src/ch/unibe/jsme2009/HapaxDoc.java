628692d0655f1e8c8929015da1ac9bdd19468239
Adrian Kuhn <akuhn@iam.unibe.ch>
2009-02-10 15:52:04 +0000

    Changed the way documents roll.

--- Students/erni/master/MeanderCaseStudies/src/ch/unibe/jsme2009/HapaxDoc.java
+++ Students/erni/master/MeanderCaseStudies/src/ch/unibe/jsme2009/HapaxDoc.java
@@ -44,4 +44,9 @@ public class HapaxDoc {
         }
     }
 
+    @Override
+    public String toString() {
+        return name + " (" + version + ")";
+    }
+
 }
--- Students/erni/master/MeanderCaseStudies/src/ch/unibe/jsme2009/JunitCaseStudy.java
+++ Students/erni/master/MeanderCaseStudies/src/ch/unibe/jsme2009/JunitCaseStudy.java
@@ -1,11 +1,13 @@
 package ch.unibe.jsme2009;
 
+import static ch.akuhn.util.Each.withIndex;
 import ch.akuhn.fame.Tower;
 import ch.akuhn.fame.parser.InputSource;
 import ch.akuhn.hapax.corpus.Corpus;
 import ch.akuhn.hapax.corpus.Document;
 import ch.akuhn.hapax.index.LatentSemanticIndex;
 import ch.akuhn.hapax.index.TermDocumentMatrix;
+import ch.akuhn.util.Each;
 import ch.deif.meander.MDS;
 
 
@@ -24,23 +26,22 @@ public class JunitCaseStudy {
     public static Corpus corpus() {
         Corpus c = new Corpus();
         for (HapaxDoc each: tower().model.all(HapaxDoc.class)) {
-            //Out.puts(each.terms);
-            c.put(new Document(each), each.terms);
+            c.add(new Document(each, each.terms));
         }
         return c;
     }
     
-    public static LatentSemanticIndex index() {
+    public static void main(String[] args) {
         TermDocumentMatrix tdm = new TermDocumentMatrix();
         tdm.addCorpus(corpus());
-        return tdm.rejectAndWeight().createIndex();
-    }
-    
-    public static void main(String[] args) {
-        LatentSemanticIndex i = index();
+        tdm = tdm.rejectAndWeight();
+        LatentSemanticIndex i = tdm.createIndex();
         MDS mds = MDS.fromCorrelationMatrix(i.documentCorrelation());
         System.out.println(mds.r0);
         System.out.println(mds.r);
+        for (Each<Document> each: withIndex(tdm.documents())) {
+            System.out.printf("%f\t%f\t%s\n", mds.x[each.index], mds.y[each.index], each.element);
+        }
     }
     
 }
