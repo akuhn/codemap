b8d4ce97c0a0f4324e236b91bc48351e0ffa31da
Adrian Kuhn <akuhn@iam.unibe.ch>
2009-02-10 14:22:10 +0000

    A Gobbler is when a person lets their nuts hang out of the zipper on their pants, then runs around in front of people screaming, "Gobble, gobble, gobble!" This refers to the resemblance of a person's nut sack and a turkeys "snood" (The flap of skin that hangs over the turkey's beak).

--- Students/erni/master/meander/trunk/src/ch/deif/meander/MDS.java
+++ Students/erni/master/meander/trunk/src/ch/deif/meander/MDS.java
@@ -1,29 +1,54 @@
 package ch.deif.meander;
 
-import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.io.PrintStream;
 
+import ch.akuhn.hapax.linalg.SymetricMatrix;
+import ch.akuhn.hapax.util.StreamGobbler;
 import ch.akuhn.util.Throw;
 
 public class MDS {
 
-    public static void main(String[] args) {
-        
-        String matrix = "# 4 2\n1 1\n1 0\n0 0\n0 1\n# -2\n";
-        
+    private static String fname() {
+        String fname = System.getenv("MDS");
+        return fname != null ? fname : "hitmds2";
+    }
+    
+    private void printMatrixOn(SymetricMatrix matrix, PrintStream out) throws IOException {
+        out.print('#'); 
+        out.print(' ');
+        out.print(-matrix.columnSize());
+        out.print(' ');
+        out.print(-matrix.rowSize());
+        out.print('\n');
+        for (int n = 0; n < matrix.rowSize(); n++) {
+            for (int m = 0; m < matrix.columnSize(); m++) {
+                out.print(' ');
+                out.print(matrix.get(n, m));
+            }
+            out.print('\n');
+        }
+        out.print("# -2"); 
+        out.close();
+    }
+    
+    public static MDS fromCorrelationMatrix(SymetricMatrix documentCorrelation) {
+        return new MDS().compute(documentCorrelation);
+    }
+
+    private MDS compute(SymetricMatrix matrix) {
         try {
-            String command = "lib/hitmds2.exe";
+            String command = fname();
             Process proc = Runtime.getRuntime().exec(command);
             new StreamGobbler(proc.getErrorStream()).start();
             new StreamGobbler(proc.getInputStream()).start();
-            OutputStreamWriter osw = new OutputStreamWriter(proc.getOutputStream());
-            osw.append(matrix);
-            osw.flush();
+            printMatrixOn(matrix, new PrintStream(proc.getOutputStream()));
             int exit = proc.waitFor();
             if (exit != 0) throw new Error(command);
         } catch (Exception ex) {
             throw Throw.exception(ex);
         }
-        
+        return this;
     }
     
 }
