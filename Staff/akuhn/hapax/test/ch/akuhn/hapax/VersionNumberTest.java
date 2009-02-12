ae5edcc31f7a71eefa3bd5f66b80decbc8c92dea
Adrian Kuhn <akuhn@iam.unibe.ch>
2009-02-12 21:27:59 +0000

    Moved test here.

---
+++ Staff/akuhn/hapax/test/ch/akuhn/hapax/VersionNumberTest.java
@@ -0,0 +1,70 @@
+package ch.akuhn.hapax;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import ch.akuhn.hapax.corpus.VersionNumber;
+
+public class VersionNumberTest {
+
+    VersionNumber num, a, b; 
+    
+    @Test
+    public void testOne() {
+        num = new VersionNumber("6");
+        assertEquals(1, num.numbers.length);
+        assertEquals(6, num.numbers[0]);
+    }
+
+    @Test
+    public void testTwo() {
+        num = new VersionNumber("6.10");
+        assertEquals(2, num.numbers.length);
+        assertEquals(6, num.numbers[0]);
+        assertEquals(10, num.numbers[1]);
+    }
+    
+    @Test
+    public void testThree() {
+        num = new VersionNumber("6.10.4");
+        assertEquals(3, num.numbers.length);
+        assertEquals(6, num.numbers[0]);
+        assertEquals(10, num.numbers[1]);
+        assertEquals(4, num.numbers[2]);
+    }
+    
+    @Test
+    public void compareTwo() {
+        a = new VersionNumber("6.10.4");
+        b = new VersionNumber("6.11.4");
+        assertTrue(a.compareTo(b) < 0);
+        assertTrue(b.compareTo(a) > 0);
+    }
+    
+    @Test
+    public void compareOne() {
+        a = new VersionNumber("7.5.10");
+        b = new VersionNumber("6.8.11");
+        assertTrue(a.compareTo(b) > 0);
+        assertTrue(b.compareTo(a) < 0);
+    }
+
+    @Test
+    public void compareThree() {
+        a = new VersionNumber("6.10.5");
+        b = new VersionNumber("6.10.0");
+        assertTrue(a.compareTo(b) > 0);
+        assertTrue(b.compareTo(a) < 0);
+    }
+
+    @Test
+    public void compareThree2() {
+        a = new VersionNumber("6.10.5");
+        b = new VersionNumber("6.10");
+        assertTrue(a.compareTo(b) > 0);
+        assertTrue(b.compareTo(a) < 0);
+    }
+
+}
