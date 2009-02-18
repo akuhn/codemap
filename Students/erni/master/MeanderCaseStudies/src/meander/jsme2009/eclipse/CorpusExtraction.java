1adedef56d762d8037a8fd12b53f292f6d9536d5
David Erni <erni@students.unibe.ch>
2009-02-18 16:36:37 +0000

    trimming matrix to save mem

--- Students/erni/master/MeanderCaseStudies/src/meander/jsme2009/eclipse/CorpusExtraction.java
+++ Students/erni/master/MeanderCaseStudies/src/meander/jsme2009/eclipse/CorpusExtraction.java
@@ -1,9 +1,9 @@
 package meander.jsme2009.eclipse;
 
-import java.io.File;
-
 import ch.akuhn.hapax.corpus.Corpus;
+import ch.akuhn.hapax.corpus.Document;
 import ch.akuhn.hapax.index.TermDocumentMatrix;
+import ch.akuhn.util.SizeOf;
 
 public class CorpusExtraction {
 
@@ -12,11 +12,18 @@ public class CorpusExtraction {
         // XXX must run with -Xmx128M for greater justice.
         
         Corpus corpus = new Corpus();
-        corpus.importAllZipArchivesPackageWise(new File("data/eclipse"), ".java");
+        corpus.importZipArchivePackageWise("data/eclipse/eclipse-sourceBuild-srcIncluded-2.0.1.zip", ".java");
+       // corpus.importAllZipArchivesPackageWise(new File("data/eclipse"), ".java");
         System.out.println(corpus);
+        System.out.println(SizeOf.deepSizeOf(corpus));
         TermDocumentMatrix tdm = new TermDocumentMatrix();
         tdm.addCorpus(corpus);
-        System.out.println(tdm.density());
+        for(Document each: corpus.documents()) {
+            each.dropTerms();
+        }
+        tdm.trim();
+        System.out.println(SizeOf.deepSizeOf(tdm));
+        //System.out.println(tdm.density());
         
     }
     
