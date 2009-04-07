5bc3d962f5ee63d74d1fd364bf87aae69c151472
David Erni <erni@students.unibe.ch>
2009-04-07 15:05:40 +0000

    (Empty commit message)

--- Students/erni/master/softwaremap/trunk/src/ch/deif/softwaremap/views/MapView.java
+++ Students/erni/master/softwaremap/trunk/src/ch/deif/softwaremap/views/MapView.java
@@ -1,15 +1,16 @@
 package ch.deif.softwaremap.views;
 
 
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.ui.part.*;
-import org.eclipse.jface.viewers.*;
+import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.ITableLabelProvider;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerSorter;
 import org.eclipse.swt.graphics.Image;
-import org.eclipse.jface.action.*;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.ui.*;
-import org.eclipse.swt.widgets.Menu;
-import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.ISharedImages;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.ViewPart;
 
 import ch.deif.meander.ui.SoftwareMap;
 
