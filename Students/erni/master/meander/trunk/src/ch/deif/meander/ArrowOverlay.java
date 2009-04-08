ab90a75fa04e08e5c645be592e7552ecdf1b3e4a
Adrian Kuhn <akuhn@iam.unibe.ch>
2009-04-08 00:48:01 +0000

    (Empty commit message)

--- Students/erni/master/meander/trunk/src/ch/deif/meander/ArrowOverlay.java
+++ Students/erni/master/meander/trunk/src/ch/deif/meander/ArrowOverlay.java
@@ -36,14 +36,23 @@ public class ArrowOverlay extends MapVisualization {
             float y1 = from.y * pg.height;
             float x2 = to.x * pg.width;
             float y2 = to.y * pg.height;
+            float w = 40.0f;
+            pg.strokeWeight(w);
             pg.line(x1, y1, x2, y2);
             pg.pushMatrix();
             pg.translate(x2, y2);
             float a = (float) Math.atan2(x1-x2, y2-y1);
             pg.rotate(a);
-            pg.line(0, 0, -8, -12);
-            pg.line(0, 0, +8, -12);
+            pg.beginShape();
+            pg.vertex(w * -1.5f, w * -2f);
+            pg.vertex(0,0);
+            pg.vertex(w * +1.5f, w * -2f);
+            pg.endShape();
             pg.popMatrix();
+            pg.strokeWeight(1.0f);  
+            pg.fill(255);
+            pg.rect(x1-2,y1-2,4,4);
+            pg.rect(x1-100,y1-100,200,200);
         }
 
         public Location from, to;
