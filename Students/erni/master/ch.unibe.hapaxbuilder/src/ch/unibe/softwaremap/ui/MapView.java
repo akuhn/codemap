b7c6219d4d385d7fb05733dbbfd2b1b34db09155
Adrian Kuhn <akuhn@iam.unibe.ch>
2009-04-22 14:56:54 +0000

    (Empty commit message)

--- Students/erni/master/ch.unibe.hapaxbuilder/src/ch/unibe/softwaremap/ui/MapView.java
+++ Students/erni/master/ch.unibe.hapaxbuilder/src/ch/unibe/softwaremap/ui/MapView.java
@@ -9,44 +9,14 @@ import org.eclipse.ui.ISharedImages;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.ViewPart;
 
-import ch.deif.meander.ui.SoftwareMap;
-
-
-/**
- * This sample class demonstrates how to plug-in a new
- * workbench view. The view shows data obtained from the
- * model. The sample creates a dummy model on the fly,
- * but a real implementation would connect to the model
- * available either in this or another plug-in (e.g. the workspace).
- * The view is connected to the model using a content provider.
- * <p>
- * The view uses a label provider to define how model
- * objects should be presented in the view. Each
- * view can present the same model objects using
- * different labels and icons, if needed. Alternatively,
- * a single label provider can be shared between views
- * in order to ensure that objects of the same type are
- * presented in the same way everywhere.
- * <p>
- */
+import ch.deif.meander.ui.EclipseProcessingBridge;
 
 public class MapView extends ViewPart {
 
-    /**
-     * The ID of the view as specified by the extension.
-     */
     public static final String ID = MapView.class.getName();
-    private SoftwareMap softwareMap;
+    private EclipseProcessingBridge softwareMap;
 
-    /*
-     * The content provider class is responsible for
-     * providing objects to the view. It can wrap
-     * existing objects in adapters or simply return
-     * objects as-is. These objects may be sensitive
-     * to the current input of the view, or ignore
-     * it and always show the same content 
-     * (like Task List, for example).
-     */  
+
     class ViewLabelProvider extends LabelProvider implements ITableLabelProvider {
         public String getColumnText(Object obj, int index) {
             return getText(obj);
@@ -54,29 +24,25 @@ public class MapView extends ViewPart {
         public Image getColumnImage(Object obj, int index) {
             return getImage(obj);
         }
+        @Override
         public Image getImage(Object obj) {
             return PlatformUI.getWorkbench().
                     getSharedImages().getImage(ISharedImages.IMG_OBJ_ELEMENT);
         }
     }
 
-    /**
-     * The constructor.
-     */
+
     public MapView() {
     }
 
-    /**
-     * This is a callback that will allow us
-     * to create the viewer and initialize it.
-     */
+
+    @Override
     public void createPartControl(Composite parent) {
-        softwareMap = new SoftwareMap(parent);
+        softwareMap = new EclipseProcessingBridge(parent);
     }   
 
-    /**
-     * Passing the focus request to the viewer's control.
-     */
+
+    @Override
     public void setFocus() {
         softwareMap.setFocus();
     }
