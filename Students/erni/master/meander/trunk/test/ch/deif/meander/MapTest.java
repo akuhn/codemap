eb701bd8045c91f10dc5720e26675689d9b81493
Adrian Kuhn <akuhn@iam.unibe.ch>
2009-04-28 14:53:53 +0000

    Fixed tests!

--- Students/erni/master/meander/trunk/test/ch/deif/meander/MapTest.java
+++ Students/erni/master/meander/trunk/test/ch/deif/meander/MapTest.java
@@ -18,88 +18,82 @@ import ch.unibe.jexample.JExample;
 
 @RunWith(JExample.class)
 public class MapTest {
-
-    @Test
-    public Map fiveOnThreeMap() {
-        Map map = Map.builder().size(5).location(0.5, 0.5, 100).done();
-        assertEquals(5, map.getWidth());
-        assertEquals(3, map.getHeight());
-        assertEquals(1, map.locationCount());
-        return map;
-    }
-
-    @Test
-    @Given("#fiveOnThreeMap")
-    public void testPixelCoordinates(Map map) {
-        Iterator<Pixel> it = map.pixels().iterator();
-        Pixel p;
-        p = it.next();
-        assertEquals(0.0, p.x(), NaN);
-        assertEquals(0.0, p.y(), NaN);
-        p = it.next();
-        assertEquals(0.25, p.x(), NaN);
-        assertEquals(0.0, p.y(), NaN);
-        p = it.next();
-        assertEquals(0.5, p.x(), NaN);
-        assertEquals(0.0, p.y(), NaN);
-        p = it.next();
-        assertEquals(0.75, p.x(), NaN);
-        assertEquals(0.0, p.y(), NaN);
-        p = it.next();
-        assertEquals(1.0, p.x(), NaN);
-        assertEquals(0.0, p.y(), NaN);
-        p = it.next();
-        assertEquals(0.0, p.x(), NaN);
-        assertEquals(0.5, p.y(), NaN);
-        p = it.next();
-        assertEquals(0.25, p.x(), NaN);
-        assertEquals(0.5, p.y(), NaN);
-    }
-
-    @Test
-    @Given("#fiveOnThreeMap")
-    public Map mapWithDEM(Map map) {
-        assertEquals(false, map.hasDEM());
-        new DEMAlgorithm(map).run();
-        assertEquals(true, map.hasDEM());
-        return map;
-    }
-
-    @Test
-    @Given("#mapWithDEM")
-    public void testPixelElevation(Map map) {
-        Pixel p;
-        p = map.get(2, 1);
-        assertEquals(0.5, p.x(), NaN);
-        assertEquals(0.5, p.y(), NaN);
-        assertEquals(100, p.elevation(), 1e-2);
-    }
-    
-    @Test
-    public Map makeSampleMap() {
-        MapBuilder builder = Map.builder().size(200);
-        for (int a = 5; a < 90; a += 10) {
-            double rad = Math.PI / 180 * a;
-            builder.location(0.8*sin(rad), 0.8*cos(rad), a + 25);
-        }
-        return builder.done();
-    }
-    
-    @Test 
-    @Given("#makeSampleMap")
-    public Map testLocations(Map map) {
-        assertEquals(9, map.locationCount());
-        assertEquals(9, Size.of(map.locations()));
-        assertEquals(true, All.notNull(map.locations()));
-        return map;
-    }
-    
-    @Test
-    @Given("#makeSampleMap")
-    public Map testPixelDimension(Map map) {
-        assertEquals(200, map.getWidth());
-        assertEquals(200, map.getHeight());
-        return map;
-    }
-    
+	
+	@Test
+	public Map fiveOnThreeMap() {
+		Map map = Map.builder().size(5).location(0.5, 0.5, 100).done();
+		assertEquals(5, map.getWidth());
+		assertEquals(5, map.getHeight());
+		assertEquals(1, map.locationCount());
+		return map;
+	}
+	
+	@Test
+	@Given("#fiveOnThreeMap")
+	public void testPixelCoordinates(Map map) {
+		Iterator<Pixel> it = map.pixels().iterator();
+		Pixel p;
+		p = it.next();
+		assertEquals(0.0, p.x(), NaN);
+		assertEquals(0.0, p.y(), NaN);
+		p = it.next();
+		assertEquals(0.25, p.x(), NaN);
+		assertEquals(0.0, p.y(), NaN);
+		p = it.next();
+		assertEquals(0.5, p.x(), NaN);
+		assertEquals(0.0, p.y(), NaN);
+		p = it.next();
+		assertEquals(0.75, p.x(), NaN);
+		assertEquals(0.0, p.y(), NaN);
+		p = it.next();
+		assertEquals(1.0, p.x(), NaN);
+		assertEquals(0.0, p.y(), NaN);
+	}
+	
+	@Test
+	@Given("#fiveOnThreeMap")
+	public Map mapWithDEM(Map map) {
+		assertEquals(false, map.hasDEM());
+		new DEMAlgorithm(map).run();
+		assertEquals(true, map.hasDEM());
+		return map;
+	}
+	
+	@Test
+	@Given("#mapWithDEM")
+	public void testPixelElevation(Map map) {
+		Pixel p;
+		p = map.get(2, 2);
+		assertEquals(0.5, p.x(), NaN);
+		assertEquals(0.5, p.y(), NaN);
+		assertEquals(100, p.elevation(), 1e-2);
+	}
+	
+	@Test
+	public Map makeSampleMap() {
+		MapBuilder builder = Map.builder().size(200);
+		for (int a = 5; a < 90; a += 10) {
+			double rad = Math.PI / 180 * a;
+			builder.location(0.8*sin(rad), 0.8*cos(rad), a + 25);
+		}
+		return builder.done();
+	}
+	
+	@Test
+	@Given("#makeSampleMap")
+	public Map testLocations(Map map) {
+		assertEquals(9, map.locationCount());
+		assertEquals(9, Size.of(map.locations()));
+		assertEquals(true, All.notNull(map.locations()));
+		return map;
+	}
+	
+	@Test
+	@Given("#makeSampleMap")
+	public Map testPixelDimension(Map map) {
+		assertEquals(200, map.getWidth());
+		assertEquals(200, map.getHeight());
+		return map;
+	}
+	
 }
