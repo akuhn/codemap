f0928e68949a27fc039aab871268d19cbca1e45e
Adrian Kuhn <akuhn@iam.unibe.ch>
2009-04-29 19:36:59 +0000

    Fixed case of hill center outside bounds (but needs better solution).

--- Students/erni/master/meander/trunk/src/ch/deif/meander/DEMAlgorithm.java
+++ Students/erni/master/meander/trunk/src/ch/deif/meander/DEMAlgorithm.java
@@ -36,52 +36,55 @@ public class DEMAlgorithm extends MapAlgorithm {
 	private void elevateHill(Location each, float[][] offscreen) {
 		final int length = DEM.length;
 		final int radius = offscreen.length;
+		// TODO yikes, starting point can be outside the screen!
+		int y0 = Math.max(0,Math.min(length-1,each.py()));
+		int x0 = Math.max(0,Math.min(length-1,each.px()));
 		// draw north-eastern pie, row by row
-		for (int y = each.py(), n = 0; y >= 0 && n < radius; y--, n++) {
-			for (int x = each.px(), m = 0; x < length && m < n; x++, m++) {
+		for (int y = y0, n = 0; y >= 0 && n < radius; y--, n++) {
+			for (int x = x0, m = 0; x < length && m < n; x++, m++) {
 				DEM[x][y] += offscreen[n][m];
 			}
 		}
 		// draw north-western pie, row by row
-		for (int y = each.py(), n = 0; y >= 0 && n < radius; y--, n++) {
-			for (int x = each.px() - 1, m = 1; x >= 0 && m <= n; x--, m++) {
+		for (int y = y0, n = 0; y >= 0 && n < radius; y--, n++) {
+			for (int x = x0 - 1, m = 1; x >= 0 && m <= n; x--, m++) {
 				DEM[x][y] += offscreen[n][m];
 			}
 		}
 		// draw western-north pie, column by column
-		for (int x = each.px(), n = 0; x >= 0 && n < radius; x--, n++) {
-			for (int y = each.py() - 1, m = 1; y >= 0 && m < n; y--, m++) {
+		for (int x = x0, n = 0; x >= 0 && n < radius; x--, n++) {
+			for (int y = y0 - 1, m = 1; y >= 0 && m < n; y--, m++) {
 				DEM[x][y] += offscreen[n][m];
 			}
 		}
 		// draw western-south pie, column by column
-		for (int x = each.px(), n = 0; x >= 0 && n < radius; x--, n++) {
-			for (int y = each.py(), m = 0; y < length && m <= n; y++, m++) {
+		for (int x = x0, n = 0; x >= 0 && n < radius; x--, n++) {
+			for (int y = y0, m = 0; y < length && m <= n; y++, m++) {
 				DEM[x][y] += offscreen[n][m];
 			}
 		}
 		// draw south-western pie, row by row
-		for (int y = each.py(), n = 0; y < length && n < radius; y++, n++) {
-			for (int x = each.px(), m = 0; x >= 0 && m < n; x--, m++) {
+		for (int y = y0, n = 0; y < length && n < radius; y++, n++) {
+			for (int x = x0, m = 0; x >= 0 && m < n; x--, m++) {
 				DEM[x][y] += offscreen[n][m];
 			}
 		}
 		// draw south-eastern pie, row by row
-		for (int y = each.py(), n = 0; y < length && n < radius; y++, n++) {
-			for (int x = each.px() + 1, m = 1; x < length && m <= n; x++, m++) {
+		for (int y = y0, n = 0; y < length && n < radius; y++, n++) {
+			for (int x = x0 + 1, m = 1; x < length && m <= n; x++, m++) {
 				DEM[x][y] += offscreen[n][m];
 			}
 		}
 		if (PACMAN) return;
 		// draw eastern-south pie, column by column
-		for (int x = each.px(), n = 0; x < length && n < radius; x++, n++) {
-			for (int y = each.py(), m = 0; y < length && m < n; y++, m++) {
+		for (int x = x0, n = 0; x < length && n < radius; x++, n++) {
+			for (int y = y0, m = 0; y < length && m < n; y++, m++) {
 				DEM[x][y] += offscreen[n][m];
 			}
 		}
 		// draw eastern-north pie, column by column
-		for (int x = each.px(), n = 0; x < length && n < radius; x++, n++) {
-			for (int y = each.py() - 1, m = 1; y >= 0 && m <= n; y--, m++) {
+		for (int x = x0, n = 0; x < length && n < radius; x++, n++) {
+			for (int y = y0 - 1, m = 1; y >= 0 && m <= n; y--, m++) {
 				DEM[x][y] += offscreen[n][m];
 			}
 		}
@@ -109,7 +112,7 @@ public class DEMAlgorithm extends MapAlgorithm {
 	}
 
 	private float[][] makeTraingleArray() {
-		float[][] result = new float[100][0];
+		float[][] result = new float[map.getWidth()][0];
 		for (int n = 0; n < result.length; n++) result[n] = new float[n+1];
 		return result;
 	}
