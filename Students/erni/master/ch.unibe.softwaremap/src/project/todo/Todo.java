b3e5f3c73d5f97f9820cdd35640d37942a105916
David Erni <erni@students.unibe.ch>
2009-06-05 12:06:24 +0000

    todo update

--- Students/erni/master/ch.unibe.softwaremap/src/project/todo/Todo.java
+++ Students/erni/master/ch.unibe.softwaremap/src/project/todo/Todo.java
@@ -2,28 +2,30 @@ package project.todo;
 
 enum Todo {
 	
-	// XXX cleanup the map-width definition ... it's everywhere ... ahh! 
 
 	// TODO TESTS! TESTS! TEST!
+	// TODO selection as overlay
 	// TODO refresh map while editing
-	// TODO resize map to eclipse view (min-size?)
 	// TODO color overlay
-	// TODO polymetric views overlay
 	// TODO port MDS to java
 	// TODO evaluate further eclipse integration
 	// TODO search on the map
 	// TODO deal with empty projects
-	// TODO selection as overlay
+	
+	// TODO polymetric views overlay
+	// TODO overlays (eclipse search results)
 	// TODO overlays (unitTests)
 	// TODO overlays (eclemma)	
 	// TODO overlays (metrics)
 	
 	// TODO make standalone-plugin work on mac
+	// TODO cleanup the map-width definition again 
 	
 }
 
 enum Done {
 	
+	// DONE resize map to eclipse view (min-size?)
 	// DONE create update site project
 	// DONE build hapax model of project
 	// DONE link map to eclipse navigation
