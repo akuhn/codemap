82b7ba2bc7e7435f275e1015899119312fd3ff17
David Erni <erni@students.unibe.ch>
2009-06-18 13:25:00 +0000

    initial commit, started writing tests

---
+++ Students/erni/master/ch.unibe.softwaremap.test/src/ch/unibe/softwaremap/test/BaseTest.java
@@ -0,0 +1,47 @@
+package ch.unibe.softwaremap.test;
+
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.swt.widgets.Display;
+
+public abstract class BaseTest {
+	
+	/**
+	 * Process UI input but do not return for the specified time interval.
+	 * 
+	 * @param waitTimeMillis
+	 *            the number of milliseconds
+	 */
+	protected static void delay(long waitTimeMillis) {
+		Display display = Display.getCurrent();
+
+		// If this is the UI thread,
+		// then process input.
+
+		if (display != null) {
+			long endTimeMillis = System.currentTimeMillis() + waitTimeMillis;
+			while (System.currentTimeMillis() < endTimeMillis) {
+				if (!display.readAndDispatch())
+					display.sleep();
+			}
+			display.update();
+		}
+		// Otherwise, perform a simple sleep.
+
+		else {
+			try {
+				Thread.sleep(waitTimeMillis);
+			} catch (InterruptedException e) {
+				// Ignored.
+			}
+		}
+	}
+
+	/**
+	 * Wait until all background tasks are complete.
+	 */
+	public static void waitForJobs() {
+		while (!Job.getJobManager().isIdle())
+			delay(1000);
+	}
+
+}
