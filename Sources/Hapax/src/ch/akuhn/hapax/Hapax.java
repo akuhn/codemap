f135fa0d2f140f06552a6cd51d5eafa0b4f3788f
Adrian Kuhn <akuhn@iam.unibe.ch>
2009-06-22 15:11:48 +0000

    Keep the damn bar green!

--- Sources/Hapax/src/ch/akuhn/hapax/Hapax.java
+++ Sources/Hapax/src/ch/akuhn/hapax/Hapax.java
@@ -23,185 +23,186 @@ import ch.akuhn.io.chunks.WriteOnChunk;
 
 public class Hapax {
 
-	private Corpora corpora;
-	private TermDocumentMatrix buffer;
-	private LatentSemanticIndex index;
-	
-	private boolean ignoreCase;
-	private Scanner scanner;
-	private Stemmer stemmer;
-	private GlobalWeighting globalWeighting;
-	private LocalWeighting localWeighting;
-	private boolean rejectStopwords;
-	private boolean rejectRareWords;
-	private String folder;
+    private Corpora corpora;
+    private TermDocumentMatrix buffer;
+    private LatentSemanticIndex index;
 
-	private Hapax() {
-		this.index = null;
-		this.corpora = new Corpora();
-		this.buffer = null;
-	}
-	
-	public static Hapax legomenon() {
-		return new Hapax();
-	}
+    private boolean ignoreCase;
+    private Scanner scanner;
+    private Stemmer stemmer;
+    private GlobalWeighting globalWeighting = GlobalWeighting.NULL;
+    private LocalWeighting localWeighting = LocalWeighting.NULL;
+    private boolean rejectStopwords;
+    private boolean rejectRareWords;
+    private String folder = "";
 
-	public Hapax createIndex() {
-		// TODO fix this mess, these semantics or so screwed up!!!
-		TermDocumentMatrix tdm = (TermDocumentMatrix) corpora.iterator().next();
-		this.index = tdm.weight(localWeighting, globalWeighting).createIndex();
-		return this;
-	}
-	
-	public LatentSemanticIndex getIndex() {
-		this.createIndex();
-		return index;
-	}
-	
-	public Hapax dontIgnoreCase() {
-		ignoreCase = false;
-		return this;
-	}
-	public Hapax dontUseStemming() {
-		stemmer = null;
-		return this;
-	}
-	
-	public Hapax dontWeight() {
-		setGlobalWeighting(GlobalWeighting.NULL);
-		setLocalWeighting(LocalWeighting.TERM);
-		return this;
-	}
+    private Hapax() {
+        this.index = null;
+        this.corpora = new Corpora();
+        this.buffer = null;
+    }
 
-	public Ranking<Document> find(String query) {
-		return index.rankDocumentsByQuery(query).top(10);
-	}
+    public static Hapax legomenon() {
+        return new Hapax();
+    }
 
-	public Hapax ignoreCase() {
-		ignoreCase = true;
-		return this;
-	}
+    public Hapax createIndex() {
+        // TODO fix this mess, these semantics or so screwed up!!!
+        TermDocumentMatrix tdm = (TermDocumentMatrix) corpora.iterator().next();
+        this.index = tdm.weight(localWeighting, globalWeighting).createIndex();
+        return this;
+    }
 
-	public Hapax makeCorpus() {
-		closeCorpus();
-		buffer = new TermDocumentMatrix();
-		return this;
-	}
-	
-	public Hapax closeCorpus() {
-		if (buffer != null) {
-			if (ignoreCase) buffer = buffer.toLowerCase();
-			if (rejectRareWords) buffer = buffer.rejectHapaxes();
-			if (rejectStopwords) buffer = buffer.rejectStopwords();
-			if (stemmer != null) buffer = buffer.stem(stemmer);
-			corpora.add(buffer);
-		}
-		buffer = null;
-		return this;
-	}
-	
-	public Hapax setBaseFolder(String folder) {
-		this.folder = folder;
-		return this;
-	}
+    public LatentSemanticIndex getIndex() {
+        this.createIndex();
+        return index;
+    }
 
-	public Hapax makeCorpus(String source, String extensions) {
-		makeCorpus();
-		addFiles(source, extensions);
-		closeCorpus();
-		return this;
-	}
+    public Hapax dontIgnoreCase() {
+        ignoreCase = false;
+        return this;
+    }
+    public Hapax dontUseStemming() {
+        stemmer = null;
+        return this;
+    }
 
-	public Hapax addFiles(String source, String extensions) {
-		if (buffer == null) throw new IllegalStateException("Please #makeCorpus first.");
-		new CorpusBuilder(buffer).version(source).importFrom(folder + source, extensions);
-		return this;
-	}
-	
-	public Hapax rejectRareWords() {
-		rejectRareWords = true;
-		return this;
-	}
+    public Hapax dontWeight() {
+        setGlobalWeighting(GlobalWeighting.NULL);
+        setLocalWeighting(LocalWeighting.TERM);
+        return this;
+    }
 
-	public Hapax dontRejectRareWords() {
-		rejectRareWords = true;
-		return this;
-	}
-	public Hapax setGlobalWeighting(GlobalWeighting weighting) {
-		this.globalWeighting = weighting;
-		return this;
-	}
-	
-	public Hapax setLocalWeighting(LocalWeighting weighting) {
-		this.localWeighting = weighting;
-		return this;
-	}
-	
-	public Hapax useCamelCaseScanner() {
-		scanner = new CamelCaseScanner();
-		return this;
-	}
-	
-	public void useDefaults() {
-		useCamelCaseScanner();
-		useStemming();
-		ignoreCase();
-		useTFIDF();
-		rejectRareWords();
-		rejectStopwords();
-		//truncationRank();
-	}
-	
-//	private Hapax truncationRank(int rank) {
-//		// TODO Auto-generated method stub
-//		
-//	}
+    public Ranking<Document> find(String query) {
+        return index.rankDocumentsByQuery(query).top(10);
+    }
 
-	public Hapax rejectStopwords() {
-		rejectStopwords = true;
-		return this;
-	}
+    public Hapax ignoreCase() {
+        ignoreCase = true;
+        return this;
+    }
 
-	public Hapax dontRejectStopwords() {
-		rejectStopwords = true;
-		return this;
-	}
-	
-	public Hapax useLetterScanner() {
-		scanner = new LetterScanner();
-		return this;
-	}
-	
-	public Hapax useStemming() {
-		stemmer = new PorterStemmer();
-		return this;
-	}
-	
-	public Hapax useTFIDF() {
-		setGlobalWeighting(GlobalWeighting.IDF);
-		setLocalWeighting(LocalWeighting.TERM);
-		return this;
-	}
-	
-	public Hapax useWordScanner() {
-		scanner = new WordScanner();
-		return this;	
-	}
-	
-	@WriteOnChunk("HPAX")
-	public void storeOn(ChunkOutput chunk) throws IOException {
-		chunk.write(0x20090507); // version
-		chunk.writeChunk(corpora);
-	}
-	
-	@ReadFromChunk("HPAX")
-	public Hapax(ChunkInput chunk) throws IOException {
-		assert chunk.readInt() == 0x20090507;
-		corpora = chunk.readChunk(Corpora.class);
-	}
+    public Hapax makeCorpus() {
+        closeCorpus();
+        buffer = new TermDocumentMatrix();
+        return this;
+    }
 
-	public Corpora corpora() {
-		return corpora;
-	}
-	
+    public Hapax closeCorpus() {
+        if (buffer != null) {
+            if (ignoreCase) buffer = buffer.toLowerCase();
+            if (rejectRareWords) buffer = buffer.rejectHapaxes();
+            if (rejectStopwords) buffer = buffer.rejectStopwords();
+            if (stemmer != null) buffer = buffer.stem(stemmer);
+            corpora.add(buffer);
+        }
+        buffer = null;
+        return this;
+    }
+
+    public Hapax setBaseFolder(String folder) {
+        this.folder = folder;
+        return this;
+    }
+
+    public Hapax makeCorpus(String source, String extensions) {
+        makeCorpus();
+        addFiles(source, extensions);
+        closeCorpus();
+        return this;
+    }
+
+    public Hapax addFiles(String source, String extensions) {
+        if (buffer == null) throw new IllegalStateException("Please #makeCorpus first.");
+        new CorpusBuilder(buffer).version(source).importFrom(folder + source, extensions);
+        return this;
+    }
+
+    public Hapax rejectRareWords() {
+        rejectRareWords = true;
+        return this;
+    }
+
+    public Hapax dontRejectRareWords() {
+        rejectRareWords = true;
+        return this;
+    }
+    public Hapax setGlobalWeighting(GlobalWeighting weighting) {
+        this.globalWeighting = weighting;
+        return this;
+    }
+
+    public Hapax setLocalWeighting(LocalWeighting weighting) {
+        this.localWeighting = weighting;
+        return this;
+    }
+
+    public Hapax useCamelCaseScanner() {
+        scanner = new CamelCaseScanner();
+        return this;
+    }
+
+    public void useDefaults() {
+        useCamelCaseScanner();
+        useStemming();
+        ignoreCase();
+        useTFIDF();
+        rejectRareWords();
+        rejectStopwords();
+        //truncationRank();
+    }
+
+    //	private Hapax truncationRank(int rank) {
+    //		// TODO Auto-generated method stub
+    //		
+    //	}
+
+    public Hapax rejectStopwords() {
+        rejectStopwords = true;
+        return this;
+    }
+
+    public Hapax dontRejectStopwords() {
+        rejectStopwords = true;
+        return this;
+    }
+
+    public Hapax useLetterScanner() {
+        scanner = new LetterScanner();
+        return this;
+    }
+
+    public Hapax useStemming() {
+        stemmer = new PorterStemmer();
+        return this;
+    }
+
+    public Hapax useTFIDF() {
+        setGlobalWeighting(GlobalWeighting.IDF);
+        setLocalWeighting(LocalWeighting.TERM);
+        return this;
+    }
+
+    public Hapax useWordScanner() {
+        scanner = new WordScanner();
+        return this;	
+    }
+
+    @WriteOnChunk("HPAX")
+    public void storeOn(ChunkOutput chunk) throws IOException {
+        chunk.write(0x20090507); // version
+        chunk.writeChunk(corpora);
+    }
+
+    @ReadFromChunk("HPAX")
+    public Hapax(ChunkInput chunk) throws IOException {
+        int version = chunk.readInt();
+        //if (version != 0x20090507) throw new Error(Integer.toHexString(version));
+        corpora = chunk.readChunk(Corpora.class);
+    }
+
+    public Corpora corpora() {
+        return corpora;
+    }
+
 }
---
+++ Sources/Hapax/test/hapax/test/SmallDocumentsTest.java
@@ -0,0 +1,20 @@
+package hapax.test;
+
+import org.junit.Test;
+
+import ch.akuhn.hapax.corpus.Terms;
+import ch.akuhn.hapax.index.LatentSemanticIndex;
+import ch.akuhn.hapax.index.TermDocumentMatrix;
+
+public class SmallDocumentsTest {
+
+    @Test
+    public void corpusWithSmallDocuments() {
+        TermDocumentMatrix tdm = new TermDocumentMatrix();
+        tdm.makeDocument("m1").addTerms(new Terms("Lorem ipsum"));
+        tdm.makeDocument("m2").addTerms(new Terms("Lorem ipsum"));
+        LatentSemanticIndex lsi = tdm.rejectAndWeight().createIndex();
+        System.out.println(lsi);
+    }
+    
+}
