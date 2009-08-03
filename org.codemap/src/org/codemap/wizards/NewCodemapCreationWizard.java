78e811be85f1b7093a7c5e1295135d951b41d7c2
Adrian Kuhn <akuhn@iam.unibe.ch>
2009-08-03 14:16:27 +0000

    After three days of tedious SWT work, our wizard can create empty files.

--- org.codemap/src/org/codemap/wizards/NewCodemapCreationWizard.java
+++ org.codemap/src/org/codemap/wizards/NewCodemapCreationWizard.java
@@ -1,14 +1,34 @@
 package org.codemap.wizards;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+
+import javax.management.RuntimeErrorException;
+
 import org.eclipse.core.internal.resources.Workspace;
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.content.IContentType;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.IFileEditorMapping;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.ide.IDE;
 import org.eclipse.ui.internal.WorkbenchPlugin;
 
 
@@ -26,11 +46,44 @@ public class NewCodemapCreationWizard extends Wizard implements INewWizard {
 
 	@Override
 	public boolean performFinish() {
+		String containerName = page.getContainerPath();
+		String fileName = page.getFilename();
 		
-		IContentType[] allContentTypes = Platform.getContentTypeManager().getAllContentTypes();
-		IFileEditorMapping[] fileEditorMappings = WorkbenchPlugin.getDefault().getEditorRegistry().getFileEditorMappings();
+		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+		IResource resource = root.findMember(new Path(containerName));
+		IContainer container = (IContainer) resource;
+		final IFile file = container.getFile(new Path(fileName));
+		try {
+			InputStream stream = openContentStream();
+			if (file.exists()) {
+				file.setContents(stream, true, true, null);
+			} else {
+				file.create(stream, true, null);
+			}
+			stream.close();
+		} catch (Exception ex) {
+			throw new RuntimeException(ex);
+		}
+		getShell().getDisplay().asyncExec(new Runnable() {
+			public void run() {
+				try {
+					IDE.openEditor(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage(), file, true);
+				} catch (PartInitException ex) {
+					throw new RuntimeException(ex);
+				}
+			}
+		});
+		
+//		IContentType[] allContentTypes = Platform.getContentTypeManager().getAllContentTypes();
+//		IFileEditorMapping[] fileEditorMappings = WorkbenchPlugin.getDefault().getEditorRegistry().getFileEditorMappings();
 		
 		return true;
+	}
+
+	private InputStream openContentStream() {
+		String contents =
+			"All your bytes are belong to us.";
+		return new ByteArrayInputStream(contents.getBytes());
 	}
 	
 	public void init(IWorkbench workbench, IStructuredSelection selection) {
--- org.codemap/src/org/codemap/wizards/NewCodemapCreationWizardPage.java
+++ org.codemap/src/org/codemap/wizards/NewCodemapCreationWizardPage.java
@@ -5,73 +5,101 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IPackageFragmentRoot;
 import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.internal.corext.util.Messages;
+import org.eclipse.jdt.internal.ui.dialogs.StatusInfo;
 import org.eclipse.jdt.internal.ui.filters.ClosedProjectFilter;
+import org.eclipse.jdt.internal.ui.viewsupport.BasicElementLabels;
+import org.eclipse.jdt.internal.ui.wizards.NewWizardMessages;
+import org.eclipse.jdt.internal.ui.wizards.TypedElementSelectionValidator;
+import org.eclipse.jdt.internal.ui.wizards.TypedViewerFilter;
 import org.eclipse.jdt.internal.ui.wizards.dialogfields.DialogField;
 import org.eclipse.jdt.internal.ui.wizards.dialogfields.IDialogFieldListener;
 import org.eclipse.jdt.internal.ui.wizards.dialogfields.IListAdapter;
+import org.eclipse.jdt.internal.ui.wizards.dialogfields.IStringButtonAdapter;
 import org.eclipse.jdt.internal.ui.wizards.dialogfields.ListDialogField;
 import org.eclipse.jdt.internal.ui.wizards.dialogfields.Separator;
+import org.eclipse.jdt.internal.ui.wizards.dialogfields.StringButtonDialogField;
 import org.eclipse.jdt.internal.ui.wizards.dialogfields.StringDialogField;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.ILabelProvider;
-import org.eclipse.jface.viewers.ILabelProviderListener;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.StructuredSelection;
-import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.ViewerFilter;
 import org.eclipse.jface.window.Window;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.dialogs.ElementTreeSelectionDialog;
+import org.eclipse.ui.dialogs.ISelectionStatusValidator;
 import org.eclipse.ui.dialogs.ListSelectionDialog;
+import org.eclipse.ui.model.WorkbenchContentProvider;
 import org.eclipse.ui.model.WorkbenchLabelProvider;
 import org.eclipse.ui.model.WorkbenchViewerComparator;
+import org.eclipse.ui.views.navigator.ResourceComparator;
 
-import ch.akuhn.foreach.Each;
 import ch.akuhn.foreach.For;
 
 @SuppressWarnings("restriction") // TODO
-public class NewCodemapCreationWizardPage extends WizardPage implements IDialogFieldListener, IListAdapter {
+public class NewCodemapCreationWizardPage extends WizardPage implements IDialogFieldListener, IListAdapter, IStringButtonAdapter {
 
 	private ListDialogField fFileExtensionsDialogField;
-
 	private StringDialogField fMapNameDialogField;
-
 	private ListDialogField fProjectListDialogField;
+	private StringButtonDialogField fFolderNameDialogField;
+
+	private IResource fCurrProject;
 
 	protected NewCodemapCreationWizardPage(ISelection selection) {
 		super("wizardPage");
 		this.setTitle("Codemap");
 		this.setDescription("Create a new codemap.");
+		
+		initializeDialogFields();
+		initialize(selection);
+	}
 
+	private void initializeDialogFields() {
+		fFolderNameDialogField= new StringButtonDialogField(this);
+		fFolderNameDialogField.setDialogFieldListener(this);
+		fFolderNameDialogField.setLabelText("Folder name:");
+		fFolderNameDialogField.setButtonLabel("Browse...");
+		
 		fMapNameDialogField = new StringDialogField();
 		fMapNameDialogField.setDialogFieldListener(this);
 		fMapNameDialogField.setLabelText("Map name:");
 		fMapNameDialogField.setText("default.map");
 		
-		fProjectListDialogField= new ListDialogField(this, new String[] { "Add...", 	null, "Remove" },
+		fProjectListDialogField= new ListDialogField(this, 
+				new String[] { "Add...", 	null, "Remove" },
 				new WorkbenchLabelProvider());
 		fProjectListDialogField.setDialogFieldListener(this);
 		fProjectListDialogField.setLabelText("Projects:");
 		fProjectListDialogField.setRemoveButtonIndex(2);
 
-		fFileExtensionsDialogField= new ListDialogField(this, new String[] { "Add...", 	null, "Remove" },
+		fFileExtensionsDialogField= new ListDialogField(this, 
+				new String[] { "Add...", 	null, "Remove" },
 				new LabelProvider());
 		fFileExtensionsDialogField.setDialogFieldListener(this);
 		fFileExtensionsDialogField.setLabelText("File extensions:");
 		fFileExtensionsDialogField.setRemoveButtonIndex(2);
-		initialize(selection);
 	}
 	@Override
 	public void createControl(Composite parent) {
@@ -82,8 +110,8 @@ public class NewCodemapCreationWizardPage extends WizardPage implements IDialogF
 		composite.setLayout(new GridLayout(nColumns, false));
 
 		// pick & choose the wanted UI components
-
 		createMapNameControls(composite, nColumns);
+		createFolderNameControls(composite, nColumns);
 		createSeparator(composite, nColumns);
 		createProjectListControls(composite, nColumns);
 		createProjectListControls(composite, nColumns);
@@ -92,6 +120,10 @@ public class NewCodemapCreationWizardPage extends WizardPage implements IDialogF
 		setControl(composite);
 		Dialog.applyDialogFont(composite);
 		// PlatformUI.getWorkbench().getHelpSystem().setHelp(composite, IJavaHelpContextIds.NEW_CLASS_WIZARD_PAGE);
+	}
+	
+	private void createFolderNameControls(Composite composite, int nColumns) {
+		fFolderNameDialogField.doFillIntoGrid(composite, nColumns);
 	}
 	
 	@SuppressWarnings("unchecked")
@@ -103,7 +135,17 @@ public class NewCodemapCreationWizardPage extends WizardPage implements IDialogF
 			IResource resource = null;
 			if (each instanceof IResource) resource = (IResource) each;
 			else if (each instanceof IAdaptable) resource = (IResource) ((IAdaptable) each).getAdapter(IResource.class);
-			if (resource != null) projects.add(resource.getProject());
+			if (resource != null) {
+				projects.add(resource.getProject());
+				if (fCurrProject == null) {
+					fCurrProject = resource.getProject();
+					if (resource.getType() == IResource.FILE) {
+						fFolderNameDialogField.setText(resource.getParent().getFullPath().makeRelative().toString());
+					} else {
+						fFolderNameDialogField.setText(resource.getFullPath().makeRelative().toString());
+					}
+				}
+			}
 		}
 		String fileExtension = "*.txt";
 		for (Object each: projects) {
@@ -111,6 +153,7 @@ public class NewCodemapCreationWizardPage extends WizardPage implements IDialogF
 		}
 		fFileExtensionsDialogField.addElement(fileExtension);
 		for (Object each: projects) fProjectListDialogField.addElement(each);
+		fCurrProject = (IProject) projects.iterator().next();
 	}
 	
 	private void createFileExtensionsControls(Composite composite, int nColumns) {
@@ -166,22 +209,95 @@ public class NewCodemapCreationWizardPage extends WizardPage implements IDialogF
 		result.removeAll(fProjectListDialogField.getElements());
 		return result;
 	}
+
 	@Override
 	public void dialogFieldChanged(DialogField field) {
-		// TODO Auto-generated method stub
-
+		dialogChanged();
 	}
 
 	@Override
 	public void doubleClicked(ListDialogField field) {
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void selectionChanged(ListDialogField field) {
-		// TODO Auto-generated method stub
-
+		dialogChanged();
 	}
 
+	@Override
+	public void changeControlPressed(DialogField field) {
+		if (field == fFolderNameDialogField) {
+			IPath initialPath= new Path(fFolderNameDialogField.getText());
+			String title= "title";
+			String message= "message";
+			IFolder folder= chooseFolder(title, message, initialPath);
+			if (folder != null) {
+				IPath path= folder.getFullPath().makeRelative();
+				fFolderNameDialogField.setText(path.toString());
+			}
+		}
+		dialogChanged();
+	}
+	
+	
+	private boolean dialogChanged() {
+		String folderName= getContainerPath();
+		String filename= getFilename();
+
+		if (folderName.isEmpty()) return updateStatus("Folder must be specified");
+		
+		IResource container = ResourcesPlugin.getWorkspace().getRoot().findMember(folderName);
+
+		if (container == null) return updateStatus("Folder container must exist");
+		if (container.getType() == IResource.ROOT) return updateStatus("Folder must not be the root");
+		if (container.getType() == IResource.FILE) return updateStatus("Folder must not be a file");
+		if (!container.isAccessible()) return updateStatus("Folder must be writable");
+
+		if (filename.isEmpty()) return updateStatus("File must be specified");
+		if (filename.indexOf('/') >= 0 || filename.indexOf('\\') >= 0) return updateStatus("File name must be valid");
+		if (filename.indexOf('.') >= -1 && !filename.endsWith(".map")) return updateStatus("File extension must be \"map\"");
+
+		return updateStatus(null);
+	}
+
+	@SuppressWarnings("unchecked")
+	private IFolder chooseFolder(String title, String message, IPath initialPath) {
+		Class[] acceptedClasses= new Class[] { IContainer.class };
+		ViewerFilter filter= new TypedViewerFilter(acceptedClasses, null);
+
+		ILabelProvider lp= new WorkbenchLabelProvider();
+		ITreeContentProvider cp= new WorkbenchContentProvider();
+
+		ElementTreeSelectionDialog dialog= new ElementTreeSelectionDialog(getShell(), lp, cp);
+		dialog.setTitle(title);
+		dialog.setMessage(message);
+		dialog.addFilter(filter);
+		dialog.setInput(ResourcesPlugin.getWorkspace().getRoot());
+		dialog.setComparator(new ResourceComparator(ResourceComparator.NAME));
+//		IResource res= currProject.findMember(initialPath);
+//		if (res != null) {
+//			dialog.setInitialSelection(res);
+//		}
+
+		if (dialog.open() == Window.OK) {
+			return (IFolder) dialog.getFirstResult();
+		}
+		return null;
+	}	
+	
+	private boolean updateStatus(String message) {
+		setErrorMessage(message);
+		setPageComplete(message == null);
+		return message == null; 
+	}
+	
+	public String getFilename() {
+		return fMapNameDialogField.getText();
+	}
+	
+	public String getContainerPath() {
+		return fFolderNameDialogField.getText();
+	}
+	
 }
