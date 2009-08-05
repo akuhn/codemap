08be7c5eebd2e3a06c783402576559bd5962f298
David Erni <erni@students.unibe.ch>
2009-08-05 11:50:19 +0000

    switched ui elements.

--- org.codemap/src/org/codemap/mapview/MapView.java
+++ org.codemap/src/org/codemap/mapview/MapView.java
@@ -136,15 +136,14 @@ public class MapView extends ViewPart {
 	private void configureToolbar() {
 	    IToolBarManager tbm = getToolBarManager();
 	    tbm.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
-	    tbm.add(new Separator(LINK_SELECTION_GROUP));
-	    
-	    tbm.appendToGroup(LINK_SELECTION_GROUP, new LinkWithSelectionAction(selectionTracker));
-	    tbm.appendToGroup(LINK_SELECTION_GROUP, new ForceSelectionAction(selectionProvider));
-   
 	    tbm.add(new Separator());
+	    
 	    tbm.add(registerAction(new ColorDropDownAction(theController)));
 	    tbm.add(registerAction(new LayerDropDownAction()));
 	    tbm.add(registerAction(new LabelDrowDownAction()));
+	    
+	    tbm.add(new LinkWithSelectionAction(selectionTracker));
+	    tbm.add(new ForceSelectionAction(selectionProvider));
 	}
 
 	private IAction registerAction(DropDownAction action) {
