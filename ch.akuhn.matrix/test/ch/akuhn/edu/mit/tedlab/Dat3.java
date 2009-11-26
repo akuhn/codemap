ec7833e0f31c323ea320818a8b1b3990da44de8c
Adrian Kuhn <akuhn@iam.unibe.ch>
2009-11-26 23:00:15 +0000

    Copying svdlibc here.

---
+++ ch.akuhn.matrix/test/ch/akuhn/edu/mit/tedlab/Dat3.java
@@ -0,0 +1,30 @@
+package ch.akuhn.edu.mit.tedlab;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import ch.akuhn.edu.mit.tedlab.SMat;
+import ch.akuhn.edu.mit.tedlab.SVDRec;
+import ch.akuhn.edu.mit.tedlab.Svdlib;
+
+
+
+public class Dat3 {
+
+    public static void main(String... args) throws FileNotFoundException {
+        Svdlib svdlib = new Svdlib();
+        SMat A = Svdlib.svdLoadSparseTextHBFile(new File("lib/dat3.txt"));
+        int dimensions = Math.min(A.cols, A.rows);
+        int iterations = 100;
+        double[] end = new double[] { -1e-30, 1e-30 };
+        double kappa = 1e-6;
+        long time = System.currentTimeMillis();
+        SVDRec R = svdlib.svdLAS2(A, dimensions, iterations, end, kappa);
+        System.out.println("-----------" + (System.currentTimeMillis() - time));
+        for (int n = 0; n < R.d; n++) {
+            System.out.println(R.S[n]);
+        }
+    }
+
+}
+
---
+++ ch.akuhn.matrix/test/ch/akuhn/edu/mit/tedlab/SmallMatricesTest.java
@@ -0,0 +1,45 @@
+package ch.akuhn.edu.mit.tedlab;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import ch.akuhn.edu.mit.tedlab.DMat;
+import ch.akuhn.edu.mit.tedlab.SMat;
+import ch.akuhn.edu.mit.tedlab.SVDRec;
+import ch.akuhn.edu.mit.tedlab.Svdlib;
+
+public class SmallMatricesTest {
+
+    @Test
+    public void emptyMatrix() {
+        DMat d = new DMat(0,0);
+        SMat s = new Svdlib().svdConvertDtoS(d);
+        SVDRec rec = new Svdlib().svdLAS2A(s, 20);
+        assertEquals(0, rec.Ut.value.length);
+        assertEquals(0, rec.Vt.value.length);
+        assertEquals(0, rec.S.length);
+    }
+    
+    @Test
+    public void oneOnOneMatrix() {
+        DMat d = new DMat(1,1);
+        SMat s = new Svdlib().svdConvertDtoS(d);
+        SVDRec rec = new Svdlib().svdLAS2A(s, 20);
+        assertEquals(1, rec.Ut.value.length);
+        assertEquals(1, rec.Vt.value.length);
+        assertEquals(1, rec.S.length);
+    }
+
+    @Test
+    public void ThreeOnThreeMatrix() {
+        DMat d = new DMat(3,3);
+        SMat s = new Svdlib().svdConvertDtoS(d);
+        SVDRec rec = new Svdlib().svdLAS2A(s, 20);
+        assertEquals(3, rec.Ut.value.length);
+        assertEquals(3, rec.Vt.value.length);
+        assertEquals(3, rec.S.length);
+    }
+    
+    
+}
