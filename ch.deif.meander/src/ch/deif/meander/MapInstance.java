2fe01b85d8d58afaafd302a30f73b28439b253d1
David Erni <erni@students.unibe.ch>
2009-12-01 15:09:10 +0000

    cleanup.

--- ch.deif.meander/src/ch/deif/meander/MapInstance.java
+++ ch.deif.meander/src/ch/deif/meander/MapInstance.java
@@ -61,9 +61,6 @@ public class MapInstance {
             throw new RuntimeException(e);
         }
     }
-    public Pixel get(int x, int y) {
-        return new Pixel(x, y);
-    }
 
     public int getWidth() { // TODO rename to getSize()
         return width;
@@ -71,33 +68,6 @@ public class MapInstance {
 
     public <V> V get(Class<? extends MapAlgorithm<V>> key) {
         return caches.get(key);
-    }
-
-    /**
-     * warning, SLOW as hell!
-     * if you need performance get the DEM manually and use for loops.
-     * 
-     */
-    public Iterable<Kernel> kernels() {
-        return new Providable<Kernel>() {
-            private int x, y;
-
-            @Override
-            public void initialize() {
-                x = y = 0;
-            }
-
-            @Override
-            public Kernel provide() {
-                if (x >= getWidth()) {
-                    x = 0;
-                    y++;
-                }
-                if (y >= getWidth()) return done();
-                Kernel k = new Kernel(x++, y);
-                return k;
-            }
-        };
     }
 
     public Iterable<Location> locations() {
@@ -126,130 +96,6 @@ public class MapInstance {
         MapInstance result = new MapInstance(query.getResult(), width, kdTree);
         result.settings = new ConcurrentHashMap<MapSetting<?>, Object>(this.settings);
         return result;
-    }
-
-    public Iterable<Pixel> pixels() {
-        return pixelsByRows();
-    }
-    public Iterable<Pixel> pixelsByColumns() {
-        return new Providable<Pixel>() {
-            private final int fheight = getWidth();
-            private int n, m;
-            private Pixel pixel = new Pixel(0, 0);
-            private final int fwidth = getWidth();
-
-            @Override
-            public void initialize() {
-                n = m = 0;
-            }
-
-            @Override
-            public Pixel provide() {
-                if (m >= fheight) {
-                    m = 0;
-                    n++;
-                    if (n >= fwidth) return done();
-                }
-                pixel.px = n;
-                pixel.py = m++;
-                return pixel;
-            }
-        };
-    }
-
-    public Iterable<Pixel> pixelsByRows() {
-        return new Providable<Pixel>() {
-            private final int fheight = getWidth();
-            private Pixel pixel = new Pixel(0, 0);
-            private final int fwidth = getWidth();
-            private int x, y;
-
-            @Override
-            public void initialize() {
-                x = y = 0;
-            }
-
-            @Override
-            public Pixel provide() {
-                if (x >= fwidth) {
-                    x = 0;
-                    y++;
-                    if (y >= fheight) return done();
-                }
-                pixel.px = x++;
-                pixel.py = y;
-                return pixel;
-            }
-        };
-    }
-
-
-    public class Kernel extends Pixel {
-
-        public double topLeft, top, topRight, left, here, right, bottomLeft, bottom, bottomRight;
-
-        public Kernel(int x, int y) {
-            super(x, y);
-            int yTop = (y == 0 ? 0 : y - 1);
-            int xLeft = (x == 0 ? 0 : x - 1);
-            int yBottom = (y == (getWidth() - 1) ? (getWidth() - 1) : y + 1);
-            int xRight = (x == (getWidth() - 1) ? (getWidth() - 1) : x + 1);
-
-            float[][] DEM = get(DEMAlgorithm.class);
-
-            topLeft = DEM[xLeft][yTop];
-            top = DEM[x][yTop];
-            topRight = DEM[xRight][yTop];
-            left = DEM[xLeft][y];
-            here = DEM[x][y];
-            right = DEM[xRight][y];
-            bottomLeft = DEM[xLeft][yBottom];
-            bottom = DEM[x][yBottom];
-            bottomRight = DEM[xRight][yBottom];
-        }
-
-    }	
-
-    public class Pixel {
-
-        private float[][] DEM0;
-        public int px;
-        public int py;
-
-        public Pixel(int px, int py) {
-            assert px < width && py < width : px + "," + py;
-            this.px = px;
-            this.py = py;
-            this.DEM0 = get(DEMAlgorithm.class);
-        }
-
-        public double elevation() {
-            return DEM0[px][py];
-        }
-
-        public void increaseElevation(double elevation) {
-            if (elevation < 0) return;
-            DEM0[px][py] += elevation;
-        }
-
-        public MColor nearestNeighborColor() {
-            // TODO go over color scheme
-            // return NN == null ? MapColor.HILLGREEN : NN.get(px).get(py).color();
-            return MColor.HILLGREEN;
-        }
-
-        public void normalizeElevation(double maxElevation) {
-            DEM0[px][py] = (float) (100.0 * (DEM0[px][py] / maxElevation));
-        }
-
-        public double x() {
-            return (double) px / (width - 1);
-        }
-
-        public double y() {
-            return (double) py / (width - 1);
-        }
-
     }
 
     @SuppressWarnings("unchecked")
@@ -297,9 +143,11 @@ public class MapInstance {
         }
         return max;
     }
+    
     public boolean containsPoint(int x, int y) {
         return 0 <= x && 0 <= y && x < width && y < height;
     }
+    
     public boolean isEmpty() {
         return locations.isEmpty();
     }
@@ -311,5 +159,4 @@ public class MapInstance {
     public KDTree<Location> getKdTree() {
         return kdTree;
     }
-
 }
