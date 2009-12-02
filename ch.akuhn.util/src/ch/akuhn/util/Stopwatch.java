87e12223045f5b8ad9b847a1d37cd9c5ff6208aa
Adrian Kuhn <akuhn@iam.unibe.ch>
2009-12-02 01:54:02 +0000

    Working on stopwatch class.

--- ch.akuhn.util/src/ch/akuhn/util/Stopwatch.java
+++ ch.akuhn.util/src/ch/akuhn/util/Stopwatch.java
@@ -1,14 +1,20 @@
 package ch.akuhn.util;
 
+import java.io.IOException;
+
 public class Stopwatch {
 
     private int n = 0;
-    private long time = -1;
+    private long time = Long.MAX_VALUE;
+    private long collect = 0; 
     
     public void print(String message) {
-        long diff = -(time - (time = System.nanoTime()));
+    	privatePrint((time - (time = System.nanoTime())) * -1, message);
+    }
+    
+    private void privatePrint(long nanos, String message) {
         System.out.print("\t("+n+")");
-        if (n++ > 0) System.out.print(" "+(1e-9)*diff);
+        if (nanos > 0) System.out.print(" "+(1e-9)*nanos);
         if (message != null) System.out.print(" " + message);
         System.out.println();
     }
@@ -22,5 +28,29 @@ public class Stopwatch {
     public static void p() {
         SINGELTON.print(null);
     }
+
+	public void on() {
+		time = System.nanoTime();
+	}
+	
+	public <T> T off(T t) {
+		collect += (System.nanoTime() - time);
+		time = 0;
+		return t;
+	}
+	
+	public void total(String message) {
+		privatePrint(collect, message);
+		collect = 0;
+	}
+
+	public static void enter() {
+		try {
+			System.out.print("Press enter: ");
+			System.in.read();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
