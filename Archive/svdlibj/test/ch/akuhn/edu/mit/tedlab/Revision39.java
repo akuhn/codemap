483622583cd204cd02bab72e90c8a66f0a4183e5
Adrian Kuhn <akuhn@iam.unibe.ch>
2011-11-23 01:21:03 +0000

    Added warning to SVDLIBC that results are not correct for some problem sets, and added referrer to correct code based on transcoded ARPACK.

--- Archive/svdlibj/test/ch/akuhn/edu/mit/tedlab/Revision39.java
+++ Archive/svdlibj/test/ch/akuhn/edu/mit/tedlab/Revision39.java
@@ -10,6 +10,24 @@ import java.io.FileNotFoundException;
 import java.util.Random;
 import java.util.Scanner;
 
+// # CAUTION DO NOT USE !!!
+// 
+// This implementation of SVDLIB is not correct!
+// 
+// For correct code please refer to [1]
+// 
+// This code has been manually translated the code from C to Java. We have been 
+// reported that results different from the original code for some problem sets. 
+// We have a better implementation of SVD in Java, based on a version of ARPACK 
+// that was automatically transcoded from the Fortran sources using f2j [2] it
+// is faster, works on offline matrices and results should be accurate since the
+// translation to Java was automated.
+// 
+// [1] http://bender.unibe.ch/svn/codemap/ch.akuhn.matrix/src/ch/akuhn/matrix/eigenvalues/
+// [2] http://icl.cs.utk.edu/f2j
+//
+// --Nov 2011
+
 public class Revision39 {
 
 	long[] svd_longArray(int size, boolean empty, String name) {
--- Archive/svdlibj/test/ch/akuhn/edu/mit/tedlab/SmallMatricesTest.java
+++ Archive/svdlibj/test/ch/akuhn/edu/mit/tedlab/SmallMatricesTest.java
@@ -9,6 +9,24 @@ import ch.akuhn.edu.mit.tedlab.SMat;
 import ch.akuhn.edu.mit.tedlab.SVDRec;
 import ch.akuhn.edu.mit.tedlab.Svdlib;
 
+// # CAUTION DO NOT USE !!!
+// 
+// This implementation of SVDLIB is not correct!
+// 
+// For correct code please refer to [1]
+// 
+// This code has been manually translated the code from C to Java. We have been 
+// reported that results different from the original code for some problem sets. 
+// We have a better implementation of SVD in Java, based on a version of ARPACK 
+// that was automatically transcoded from the Fortran sources using f2j [2] it
+// is faster, works on offline matrices and results should be accurate since the
+// translation to Java was automated.
+// 
+// [1] http://bender.unibe.ch/svn/codemap/ch.akuhn.matrix/src/ch/akuhn/matrix/eigenvalues/
+// [2] http://icl.cs.utk.edu/f2j
+//
+// --Nov 2011
+
 public class SmallMatricesTest {
 
     @Test
